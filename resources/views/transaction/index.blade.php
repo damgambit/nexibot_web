398e249b61d4c877fc133341c474b1103436dde2
damgambit <damianogambitta@gmail.com>
2018-03-17 17:20:36 +0100

    dd

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -8,34 +8,34 @@
 
 		</div>
 
+		<div class="col-lg-6">
+			<table class="table table-bordered table-hover table-responsive">
 
-		<table class="table table-bordered table-hover table-responsive col-lg-8">
+				<thead>
+					<th>id</th>
+					<th>telegram_user_id</th>
+					<th>amount</th>
+					<th>currency</th>
+					<th>status</th>
+					<th>product_id</th>
 
-			<thead>
-				<th>id</th>
-				<th>telegram_user_id</th>
-				<th>amount</th>
-				<th>currency</th>
-				<th>status</th>
-				<th>product_id</th>
+				</thead>
 
-			</thead>
+				<tbody>
+					@foreach($transactions as $transaction)
+						<tr>
+							<td>{{$transaction->id}}</td>
+							<td>{{$transaction->telegram_user_id}}</td>
+							<td>{{$transaction->amount}}</td>
+							<td>{{$transaction->currency}}</td>
+							<td>{{$transaction->status}}</td>
+							<td>{{$transaction->product_id}}</td>
+						</tr>
+					@endforeach
+				</tbody>
 
-			<tbody>
-				@foreach($transactions as $transaction)
-					<tr>
-						<td>{{$transaction->id}}</td>
-						<td>{{$transaction->telegram_user_id}}</td>
-						<td>{{$transaction->amount}}</td>
-						<td>{{$transaction->currency}}</td>
-						<td>{{$transaction->status}}</td>
-						<td>{{$transaction->product_id}}</td>
-					</tr>
-				@endforeach
-			</tbody>
-
-		</table>
-
+			</table>
+		</div>
 	</div>
     
 @endsection

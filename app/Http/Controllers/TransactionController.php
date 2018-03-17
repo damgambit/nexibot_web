9f5def021313e41f3ddc686a979e3b908495b019
damgambit <damianogambitta@gmail.com>
2018-03-17 19:53:29 +0100

    ddd

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 
 use App\Transaction;
+use App\Product;
+use App\Merchant;
 
 class TransactionController extends Controller
 
@@ -29,13 +31,15 @@ class TransactionController extends Controller
 		$transaction = Transaction::where(['telegram_user_id' => $telegram_user_id])
 							->orderBy('created_at', 'desc')
 							->first();
+		$product = Product::where(['id' => $transaction->product_id])->first();
+		$product = Merchant::where(['id' => $product->merchant_id])->first();
 
 		if($transaction->status == "OK") {
-			echo "Success";
+			echo "Success_".$transaction->id.'_'.$product->name.'_'.$transaction->amount.'_'.$merchant->name;
 		} else if ($transaction->status = "none") {
-			echo "Pending";
+			echo "Pending_".$transaction->id.'_'.$product->name.'_'.$transaction->amount.'_'.$merchant->name;
 		} else {
-			echo "Failed";
+			echo "Failed_".$transaction->id.'_'.$product->name.'_'.$transaction->amount.'_'.$merchant->name;
 		}
 
 	}

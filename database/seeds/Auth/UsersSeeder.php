51fab97747687c8402296c33ba48f48541288715
Damiano Gambitta <damianogambitta@gmail.com>
2018-03-18 01:30:27 +0100

    Update UsersSeeder.php

--- database/seeds/Auth/UsersSeeder.php
+++ database/seeds/Auth/UsersSeeder.php
@@ -48,69 +48,64 @@ class UsersSeeder extends Seeder
         DB::table('users')->insert($users);
 
 
-        // $this->truncate('products');
-        // $this->truncate('merchants');
-        // $products = [
-        //     [
-        //         'merchant_id' => 1,
-        //         'name' => 'urbano_novanta_minuti',
-        //         'price' => 1.5
-        //     ],
-        //     [
-        //         'merchant_id' => 1,
-        //         'name' => 'urbano_settimale',
-        //         'price' => 14
-        //     ],
-        //     [
-        //         'merchant_id' => 2,
-        //         'name' => 'biglietto_normale',
-        //         'price' => 7
-        //     ],
-        //     [
-        //         'merchant_id' => 2,
-        //         'name' => 'biglietto_speciale',
-        //         'price' => 10
-        //     ],
-        //     [
-        //         'merchant_id' => 3,
-        //         'name' => 'parcheggio_ore',
-        //         'price' => 1
-        //     ],
-        //     [
-        //         'merchant_id' => 4,
-        //         'name' => 'benzina',
-        //         'price' => '0'
-        //     ]
-        // ];
-
-        // DB::table('products')->insert($products);
-
-
-
-        // $merchants = [
-        //     [
-        //         'name' => 'ATM',
-        //         'alias' => 'ALIAS_RICO_00005086',
-        //         'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5',
-        //     ], 
-        //     [
-        //         'name' => 'The Space Cinema',
-        //         'alias' => 'ALIAS_RICO_00005086',
-        //         'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
-        //     ], 
-        //     [
-        //         'name' => 'parking',
-        //         'alias' => 'ALIAS_RICO_00005086',
-        //         'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
-        //     ],
-        //     [
-        //         'name' => 'GAS',
-        //         'alias' => 'ALIAS_RICO_00005086',
-        //         'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
-        //     ], 
-        // ];
-
-        // DB::table('merchants')->insert($merchants);
+        $this->truncate('products');
+        $this->truncate('merchants');
+        $products = [
+             [
+                 'merchant_id' => 1,
+                'name' => 'urbano_novanta_minuti',
+               'price' => 1.5
+           ],
+           [
+               'merchant_id' => 1,
+               'name' => 'urbano_settimale',
+               'price' => 14
+           ],
+           [
+               'merchant_id' => 2,
+                'name' => 'biglietto_normale',
+                'price' => 7
+            ],
+            [
+                'merchant_id' => 2,
+                'name' => 'biglietto_speciale',
+                'price' => 10
+          ],
+             [
+                 'merchant_id' => 3,
+                 'name' => 'parcheggio_ore',
+                 'price' => 1
+             ],
+             [
+                 'merchant_id' => 4,
+               'name' => 'benzina',
+                 'price' => '0'
+             ]
+         ]
+         DB::table('products')->insert($products)
+        $merchants = [
+             [
+                 'name' => 'ATM',
+               'alias' => 'ALIAS_RICO_00005086',
+                 'secret' => 'QDUUZFXRG6SEZ26OY81CJDES73U3Y5',
+             ], 
+             [
+                 'name' => 'The Space Cinema',
+                 'alias' => 'ALIAS_RICO_00005086',
+                 'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
+             ], 
+             [
+                 'name' => 'parking',
+                 'alias' => 'ALIAS_RICO_00005086',
+                 'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
+             ],
+             [
+                 'name' => 'GAS',
+                 'alias' => 'ALIAS_RICO_00005086',
+               'secret' => 'QDUUZFXRG6SEZ26OYSE81CJDES73U3Y5'
+             ], 
+         ];
+        DB::table('merchants')->insert($merchants);
 
 
 
